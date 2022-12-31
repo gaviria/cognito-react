c5aa9cdfe63ab125b8865c98ea884cb3b2b7af71
Francisco <gaviria.reyes@gmail.com>
2022-12-31 10:50:01 -0500

    creating settings

--- src/components/Login.tsx
+++ src/components/Login.tsx
@@ -4,7 +4,7 @@ import { AccountContext } from "./Account";
 const Login = () => {
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
-    console.log(AccountContext);
+
     const { authenticate } = useContext(AccountContext);
 
     const onSubmit = (event) => {
---
+++ src/components/Settings.tsx
@@ -0,0 +1,29 @@
+import { useContext, useEffect, useState } from "react";
+import { AccountContext } from "./Account";
+
+const Settings = () => {
+    const { getSession } = useContext(AccountContext);
+    const [loggedin, setLoggedIn] = useState(false);
+
+    useEffect(() => {
+        getSession()
+            .then(() => {
+                setLoggedIn(true);
+            })
+            .catch((err) => {
+                console.log(err);
+            });
+    }, []);
+
+    return (
+        <div>
+            {loggedin && (
+                <>
+                    <h2>Setting</h2>
+                </>
+            )}
+        </div>
+    );
+};
+
+export default Settings;
